b663455259e48913d44c19d398a7d171d4861eea
LE MEILLEUR ET PLUS GROS KEBAB DE PARIS <bmickael521@gmail.com>
2026-09-25 09:56:00 +0000

    push

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,67 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int num1 = 12;
+    int num2 = 5;
+    char op = '+';
+    int resultat;
+
+    switch (op)
+    {
+        case '+':
+            resultat = num1 + num2;
+            printf("%d + %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '-':
+            resultat = num1 - num2;
+            printf("%d - %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '*':
+            resultat = num1 * num2;
+            printf("%d * %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '/':
+            if (num2 == 0)
+                printf("Erreur : division par zero\n");
+            else
+            {
+                resultat = num1 / num2;
+                printf("%d / %d = %d\n", num1, num2, resultat);
+            }
+            break;
+
+        case '%':
+            if (num2 == 0)
+                printf("Erreur : modulo par zero\n");
+            else
+            {
+                resultat = num1 % num2;
+                printf("%d %% %d = %d\n", num1, num2, resultat);
+            }
+            break;
+
+        case '&':
+            resultat = num1 & num2;
+            printf("%d & %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '|':
+            resultat = num1 | num2;
+            printf("%d | %d = %d\n", num1, num2, resultat);
+            break;
+
+        case '~':
+            resultat = ~num1;
+            printf("~%d = %d\n", num1, resultat);
+            break;
+
+        default:
+            printf("Operateur inconnu : %c\n", op);
+    }
+
+    return 0;
+}
