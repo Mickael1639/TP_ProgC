02202747a1e05853dd37a3d3dba28fd2f7bb383a
LE MEILLEUR ET PLUS GROS KEBAB DE PARIS <bmickael521@gmail.com>
2026-09-25 09:57:40 +0000

    push

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int n = 5;
+    int i, j;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n - i; j++)
+            printf(" ");
+
+        for (j = 1; j <= i; j++)
+            printf("%d", j % 10);
+
+        for (j = i - 1; j >= 1; j--)
+            printf("%d", j % 10);
+
+        printf("\n");
+    }
+
+    printf("Generation de la pyramide terminee.\n");
+
+    return 0;
+}
